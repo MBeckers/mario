efa4ecc6520b8662738cacc584c9e0db86089b73
Marius Beckers <marius@beckers.de>
2021-11-03 21:38:29 +0100

    added gravity and collision

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
 let marioX = 50;
-let marioY = 175;
+let marioY = 100;
+let marioAccX = 0;
+let marioAccY = 0;
+let canvas;
+let context;
+
+window.onload = init;
+
+function init(){
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+
+    // Start the first frame request
+    window.requestAnimationFrame(gameLoop);
+}
+
+function gameLoop(timeStamp){
+    gravity();
+    collision();
+    render();
+
+    // Keep requesting new frames
+    window.requestAnimationFrame(gameLoop);
+}
+
+function gravity() {
+    marioAccY = Math.min(10, marioAccY+=0.5 )
+    console.log(marioAccY);
+
+    if (!collision()) {
+        marioY = marioY + marioAccY;
+    } else {
+        marioAccY=0;    
+        marioY = 175
+    }
+
+}
+
+function collision() {
+    return marioY > 175
+}
 
 function render() {
-    var canvas = document.getElementById("myCanvas");
+    var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext('2d');
     var mario  = document.getElementById("mario");
     ctx.fillStyle = "white";
@@ -11,12 +51,10 @@ function render() {
     ctx.drawImage(mario, 50, marioY, 32, 32);
 }
 
-render();
-
 document.onkeypress = function (e) {
     e = e || window.event;
     if (e.keyCode == 119) {
-        marioY = marioY - 10;
+        marioAccY = -10;
     }
     if (e.keyCode == 115) {
         marioY = marioY + 10;
